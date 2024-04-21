7371912cbfe87da98b6ed474bafce5313a07c955
Kateryna02 <150830031+Kateryna02@users.noreply.github.com>
2024-04-21 21:07:19 +0200

    new

--- src/components/contactsList/ContactsList.jsx
+++ src/components/contactsList/ContactsList.jsx
@@ -2,27 +2,25 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ContactItem from '../ContactsItem/ContactsItem';
-import { deleteContact } from '../../redux/contactsOps';
+import { deleteContact, fetchContacts } from '../../redux/contactsOps';
 import { selectFilteredContacts } from '../../redux/selectors';
 import s from './contactsList.module.css';
 import { useEffect } from "react"
-import { fetchContacts } from '../../redux/contactsOps';
+
 
 
 const ContactList = () => {
 
     const filteredContacts = useSelector(selectFilteredContacts);
-
-    const handleDeleteContact = contactId => {
-        dispatch(deleteContact(contactId));
-    };
-
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(fetchContacts());
-        console.log(fetchContacts);
     }, [dispatch]);
+
+    const handleDeleteContact = contactId => {
+        dispatch(deleteContact(contactId));
+    };
     return (
         <ul className={s.contactList}>
             {filteredContacts.map(contact => (
